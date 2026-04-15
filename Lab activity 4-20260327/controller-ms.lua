176a7f49c79e6c2d65b0dcd8c50df9d6823fd7fa
Filippo Badioli <filippo.badioli@studio.unibo.it>
2026-04-15 09:35:07 -0400

    .

--- Lab activity 4-20260327/controller-ms.lua
+++ Lab activity 4-20260327/controller-ms.lua
@@ -1,16 +1,25 @@
 local vector = require "vector"
-local fields = require "fields"
 
 BASE_VELOCITY = 10
+MAX_VELOCITY = 15
+W1 = 10
+W2 = 30
+W3 = 3
+TICKS = 50
 
 local velocities = { left = BASE_VELOCITY, right = BASE_VELOCITY}
 
 local vectors = { 
-    light = { lenght = 0, angle = 0},
-    obstacle = { lenght = 0, angle = 0}
+    light = { length = 0, angle = 0},
+    obstacle = { length = 0, angle = 0},
+    random = { length = 0, angle = 0}
 }
 
+local random_vector = { length = 0, angle = 0}
+local random_counter = 0
+
 function init()
+    L = robot.wheels.axis_length
     robot.wheels.set_velocity(velocities.left, velocities.right)
 end
 
@@ -26,16 +35,67 @@ function destroy()
 end
 
 function controller()
-    vectors.light = fields.light_field()
-    vectors.obstacle = fields.obstacle_field()
+    vectors.light = compute_light_field(robot.light)
+    vectors.obstacle = compute_obstacle_field(robot.proximity)
+    vectors.random = compute_random_field()
 
+    log("Light field -> length = " .. vectors.light.length .. " angle = ".. vectors.light.angle)
+    log("Proximity field -> length = " .. vectors.obstacle.length .. " angle = ".. vectors.obstacle.angle)
     local sum = vector.vec2_polar_sum(vectors.light, vectors.obstacle)
+    sum = vector.vec2_polar_sum(sum, vectors.random)
     velocities = convert_to_linear(sum)
+    velocities = limitVelocity(velocities)
+    log("left = " .. velocities.left .. " right = " .. velocities.right)
     return velocities
 end
 
 function convert_to_linear(angular)
-    local left --TODO
-    local right --TODO
-    return left, right
+    local v = angular.length * math.cos(angular.angle)
+    local omega = angular.length * math.sin(angular.angle)
+
+    local left = v - (L/2)*omega
+    local right = v + (L/2)*omega
+    return {left = left, right = right}
+end
+
+function compute_light_field(sensors)
+    local field = { length = 0, angle = 0 }
+    local sensor_vector = { length = 0, angle = 0}
+    for i=1,#sensors do
+        sensor_vector.length = sensors[i].value
+        sensor_vector.angle = sensors[i].angle
+        field = vector.vec2_polar_sum(field, sensor_vector)
+    end
+    field.length = field.length * W1
+    return field
+end
+
+function compute_obstacle_field(sensors)
+    local field = { length = 0, angle = 0 }
+    local sensor_vector = { length = 0, angle = 0}
+    for i=1,#sensors do
+        sensor_vector.length = sensors[i].value
+        sensor_vector.angle = sensors[i].angle + math.pi
+        field = vector.vec2_polar_sum(field, sensor_vector)
+    end
+    field.length = field.length * W2
+    return field
+end
+
+function compute_random_field()
+    return { length = 5, angle = 0}
+end
+
+function limitVelocity(velocities)
+    local max_val = math.max(math.abs(velocities.left), math.abs(velocities.right))
+
+    if max_val > MAX_VELOCITY then
+        local scale = MAX_VELOCITY / max_val
+        return {
+            left = velocities.left * scale,
+            right = velocities.right * scale
+        }
+    else
+        return velocities
+    end
 end
--- Lab activity 4-20260327/fields.lua
+++
@@ -1,6 +0,0 @@
-function light_field(sensors)
-    
-end
-
-function obstacle_field()
-end
