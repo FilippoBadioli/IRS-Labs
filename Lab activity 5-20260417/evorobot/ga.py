adc7eba2b1b357f074e1a9456a12180ddb0878a5
Filippo Badioli <filippo.badioli@studio.unibo.it>
2026-04-17 10:24:01 -0400

    .

--- Lab activity 5-20260417/evorobot/ga.py
+++ Lab activity 5-20260417/evorobot/ga.py
@@ -7,9 +7,9 @@ import subprocess
 GENOME_LENGTH = 50
 POP_SIZE = 10
 ELITE_SIZE = 5
-GENERATIONS = 5
-MUTATION_RATE = 0.1 # prob of mutating each gene
-CX_RATE = 0 # prob of applying crossover
+GENERATIONS = 10
+MUTATION_RATE = 0.2 # prob of mutating each gene
+CX_RATE = 0.5 # prob of applying crossover
 MUTATION_INTENSITY = 1 # stddev of a Gaussian distribution with mean 0
 N_EVAL = 3
 
@@ -41,7 +41,15 @@ def fitness(individual):
 
 # Selection: tournament
 # Randomly pick 2 individuals and return the one with the highest fitness
-#def select_tournament(population,fitness_values):
+def select_tournament(population,fitness_values):
+    random1 = random.random() * len(population)
+    random2 = random.random() * len(population)
+    while random1 == random2:
+        random2 = random.random() * len(population)
+    if fitness_values[random1] > fitness_values[random2]:
+        return population[random1]
+    else:
+        return population[random2]
 
 
 # Selection: roulette wheel (proportional)
@@ -75,10 +83,10 @@ def crossover(parent1, parent2):
 # random.random() : rnd number uniformly distributed in [0,1]
 # random.gauss(0,MUTATION_INTENSITY) : rnd number in normal distrib with mean=0 and stddev=MUTATION_INTENSITY
 def mutate(individual):
-    for gene in individual:
+    for i in range(len(individual)):
         if random.random() <= MUTATION_RATE:
             mutation = random.gauss(0, MUTATION_INTENSITY)
-            gene = gene * mutation
+            individual[i] = individual[i] * mutation
     return individual
 
 
@@ -96,35 +104,44 @@ population = [create_individual() for _ in range(POP_SIZE)]
 #print("Random solution:", population[0])
 
 # REPLACEMENT
-# for gen in range(GENERATIONS):
-#     fitness_values = [fitness(i) for i in population]
-#     print_stats(population,fitness_values)
-#     new_population = []
-#     for _ in range(POP_SIZE):
-#         parent1 = select_proportional(population,fitness_values)
-#         parent2 = select_proportional(population,fitness_values)
-#         child = crossover(parent1, parent2)
-#         child = mutate(child)
-#         new_population.append(child)
-#     population = new_population
+print("REPLACEMENT")
+for gen in range(GENERATIONS):
+    fitness_values = [fitness(i) for i in population]
+    print_stats(population,fitness_values)
+    new_population = []
+    for _ in range(POP_SIZE):
+        parent1 = select_proportional(population,fitness_values)
+        parent2 = select_proportional(population,fitness_values)
+        child = crossover(parent1, parent2)
+        child = mutate(child)
+        new_population.append(child)
+    population = new_population
 
 
 # ELITISM
+print("ELITISM")
 for gen in range(GENERATIONS):
     fitness_values = [fitness(i) for i in population]
     print_stats(population,fitness_values)
 
     new_population = []
-    for _ in range(ELITE_SIZE):
-        best_individual = max(population, key=fitness)
-        idx = population.index(best_individual)
-        new_population.append(idx)
+    sorted_indices = sorted(
+        range(len(population)),
+        key=lambda i: fitness_values[i],
+        reverse=True
+    )
+    for i in sorted_indices[:ELITE_SIZE]:
+        new_population.append(population[i])
     
     for _ in range(POP_SIZE - ELITE_SIZE):
         parent1 = select_proportional(population,fitness_values)
+        if parent1 is None : print("Error: Parent 1 is None")
         parent2 = select_proportional(population,fitness_values)
+        if parent2 is None : print("Error: Parent 2 is None")
         child = crossover(parent1, parent2)
+        if child is None : print("Error: Child is None")
         child = mutate(child)
+        if child is None : print("Error: the mutation produced a None")
         new_population.append(child)
 
     population = new_population
